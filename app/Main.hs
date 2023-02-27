b40932d1f7c7ad78f0ffb89dad315629e7c96f0c
bebyx <artem@bebyx.co.ua>
2023-02-27 19:26:23 +0200

    Factor out args handling

--- app/Main.hs
+++ app/Main.hs
@@ -7,13 +7,9 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  let runeString = if null args then "Fehu" else capitalize $ args !! 0 -- ^ show random rune instead of Fehu placeholder USE GUARDS!!!
-      rune = if runeString `elem` map show futhark
-             then getDataFor $ read $ runeString
-             else error "Not a Futhark rune provided" -- ^ or just print random rune: Not a Futhark rune provided, printing random rune...
+  let rune = handleArgs args
 
   putChar ' '
   putChar $ unicode rune
   putStrLn $ " (" ++ [transliteration rune] ++ ")"
   putStrLn $ (show $ name rune) ++ ": " ++ meaning rune
-
--- src/Util.hs
+++ src/Util.hs
@@ -1,9 +1,18 @@
-module Util where
+module Util (handleArgs) where
 
 import Data.Char (toLower, toUpper)
+import Rune
 
 capitalize :: String -> String
 capitalize [] = []
 capitalize (x:xs) = toUpper x : map toLower xs
+
+handleArgs :: [String] -> RuneData
+handleArgs [] = getDataFor Fehu -- ^ show random rune instead of Fehu placeholder
+handleArgs (input:_)
+  | normalizedInput `elem` map show futhark = getDataFor $ read normalizedInput
+  | otherwise = error "Not a Futhark rune provided"
+  where
+    normalizedInput = capitalize input
 
 -- | Function to get random element of a list also goes here.
